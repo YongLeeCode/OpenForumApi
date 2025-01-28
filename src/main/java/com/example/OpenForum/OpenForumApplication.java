a0ae54130bc1e8f94b5c753792a69b5a49170a67
YongLeeCode <yonghwan311@gmail.com>
2025-01-28 23:21:09 +0900

    feat: Create OpenForum API project

---
+++ src/main/java/com/example/OpenForum/OpenForumApplication.java
@@ -0,0 +1,13 @@
+package com.example.OpenForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OpenForum/OpenForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OpenForum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
